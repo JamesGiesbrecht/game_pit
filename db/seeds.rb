5a5ffbc3966b91d1963b8dbcba35ea18bfada469
JamesGiesbrecht <giesbrechtjames@gmail.com>
2020-10-28 16:48:22 -0500

    Injecting sample products and categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,23 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# frozen_string_literal: true
+
+require "json"
+
+Category.destroy_all
+Product.destroy_all
+
+products = JSON.parse(File.read(Rails.root.join("db/data/products.json")))
+
+products.each do |p|
+  category = Category.find_or_create_by(name: p["category"])
+  product = Product.new(
+    name:           p["name"],
+    description:    p["description"],
+    price:          p["price"],
+    stock_quantity: p["stock_quantity"],
+    discount:       p["discount"],
+    category:       category
+  )
+  product.save
+end
+pp Product.take(2)
+pp Category.take(2)
