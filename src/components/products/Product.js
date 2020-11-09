20281c3fc3ad11d3387864e6fa9f33bcc45fae60
JamesGiesbrecht <giesbrechtjames@gmail.com>
2020-11-09 14:21:45 -0600

    Adding price and description to product cards

--- src/components/products/Product.js
+++ src/components/products/Product.js
@@ -1,18 +1,49 @@
 import React from 'react'
-import { Card, Typography } from 'antd'
-import { SettingOutlined, EditOutlined, EllipsisOutlined } from '@ant-design/icons'
+import { Card, Space, Typography } from 'antd'
+import { PlusOutlined, CheckOutlined, CheckCircleOutlined, StopOutlined } from '@ant-design/icons'
 
 const { Meta } = Card
 const { Text } = Typography
 
 const Product = ({ product }) => {
-  const description = () => {
+  const hasDiscount = product.discount > 0
+
+  const description = (() => {
     const d = product.details
     const possibleDescriptions = ['capacity', 'colour', 'platform', 'publisher']
     const desc = []
     possibleDescriptions.forEach((pd) => { if (d[pd]) desc.push(<Text>{d[pd]}</Text>) })
     return <Text className="product-desc">{desc.length > 2 ? [desc[0], desc[1]] : desc}</Text>
-  }
+  })()
+
+  const price = (() => {
+    const p = parseFloat(product.price).toFixed(2)
+    if (hasDiscount) {
+      return (
+        <Space direction="vertical" className="">
+          <div>
+            <Text type="danger" delete>{`$${p}`}</Text>
+            <Text type="danger" strong>{` $${(p - p * product.discount).toFixed(2)}`}</Text>
+          </div>
+          <Text type="danger">{`Save ${product.discount * 100}%`}</Text>
+        </Space>
+      )
+    }
+    return <div className="product-card-price"><Text strong>{`$${p}`}</Text></div>
+  })()
+
+  const inStock = product.stock_quantity > 0 ? (
+    <span key="inStock" style={{ color: 'green' }}>
+      <CheckOutlined />
+      {' In Stock'}
+    </span>
+  ) : (
+    <span key="inStock" style={{ color: 'red' }}>
+      <StopOutlined />
+      {' Out of Stock'}
+    </span>
+  )
+
   return (
     <Card
       className="product-card"
@@ -22,17 +53,16 @@ const Product = ({ product }) => {
           src={product.image}
         />
       )}
-      actions={[
-        <SettingOutlined key="setting" />,
-        <EditOutlined key="edit" />,
-        <EllipsisOutlined key="ellipsis" />,
-      ]}
     >
       <Meta
         title={product.name}
-        description={description()}
+        description={description}
       />
-      <Text>$39.99</Text>
+      {price}
+      <div className="product-desc">
+        <PlusOutlined key="addToCart" />
+        {inStock}
+      </div>
     </Card>
   )
 }
