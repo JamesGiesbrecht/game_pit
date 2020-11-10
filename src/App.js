67a674cc1b24ad795704c2a5f54e5c04435f4af2
JamesGiesbrecht <giesbrechtjames@gmail.com>
2020-11-10 10:22:12 -0600

    Adding React Router to project

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import Layout from 'components/Layout'
 import Products from 'components/products'
+import { BrowserRouter, Route } from 'react-router-dom'
 import 'App.css'
 import 'Ant.css'
 
 const App = () => (
-  <Layout>
-    <Products />
-  </Layout>
+  <BrowserRouter>
+      <Layout>
+        <Route path="/products" component={Products} />
+      </Layout>
+  </BrowserRouter>
 )
 
 export default App
