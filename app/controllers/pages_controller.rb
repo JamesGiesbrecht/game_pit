177fb8d12e36429c10a57456f8386530b9e85ea8
JamesGiesbrecht <giesbrechtjames@gmail.com>
2020-11-10 08:43:32 -0600

    Removing html routes for pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,18 @@
 class PagesController < InheritedResources::Base
 
+  # GET /page.json
+  def index
+    @pages = Page.all
+  end
+
+  # GET /page/1.json
+  def show
+    @page = Page.find(params[:id])
+  end
+
   private
 
-    def page_params
-      params.require(:page).permit(:title, :content, :permalink)
-    end
-
+  def page_params
+    params.require(:page).permit(:title, :content, :permalink)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  resources :pages
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   constraints ->(request) { request.format == :json } do
     resources :products
+    resources :pages
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
