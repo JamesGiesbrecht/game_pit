9c4d6c679328e4638499d817b46422dc837f5907
JamesGiesbrecht <giesbrechtjames@gmail.com>
2020-11-10 09:26:57 -0600

    Adding page links to Nav

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,5 +1,6 @@
-import React, { useState, Children, cloneElement } from 'react'
+import React, { useState, useEffect, Children, cloneElement } from 'react'
 import { Grid, Layout, Menu, Breadcrumb, Image } from 'antd'
+import axios from 'axios'
 import wideLogo from 'assets/img/logo-blue.png'
 
 const { useBreakpoint } = Grid
@@ -8,8 +9,29 @@ const { Header, Content, Footer } = Layout
 const MyLayout = ({ children }) => {
   const [breadcrumbs, setBreadcrumbs] = useState(['Home'])
   const [shoppingCart, setShoppingCart] = useState([])
+  const [pages, setPages] = useState([])
+  const [navItems, setNavItems] = useState()
+
+  useEffect(() => {
+    axios.get('/pages.json')
+      .then((res) => {
+        console.log(res)
+        setPages(res.data)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }, [])
+
+  useEffect(() => {
+    const items = [
+      // Add hardcoded links here
+    ].concat(pages.map((page) => <Menu.Item key={page.id}>{page.title}</Menu.Item>))
+    setNavItems(items)
+  }, [pages])
 
   const crumbs = breadcrumbs.map((bc) => <Breadcrumb.Item key={bc}>{bc}</Breadcrumb.Item>)
+
   const contentPadding = !useBreakpoint().xs ? '0 40px' : '0'
 
   const addItemToShoppingCart = (product) => setShoppingCart((prev) => [...prev, product])
@@ -17,15 +39,12 @@ const MyLayout = ({ children }) => {
   const childrenWithProps = Children.map(children, (child) => (
     cloneElement(child, { setBreadcrumbs, addItemToShoppingCart, shoppingCart })
   ))
-
   return (
     <Layout className="layout">
       <Header className="header">
         <Menu mode="horizontal" className="nav-menu">
           <Image src={wideLogo} className="logo" />
-          <Menu.Item key="1">nav 1</Menu.Item>
-          <Menu.Item key="2">nav 2</Menu.Item>
-          <Menu.Item key="3">nav 3</Menu.Item>
+          {navItems}
         </Menu>
       </Header>
       <Content style={{ padding: contentPadding }} className="all-content">
