6693e7c6caf9a6f1cd4b6ba91c5e1f8de15cf305
JamesGiesbrecht <giesbrechtjames@gmail.com>
2020-11-11 18:33:24 -0600

    Adding additional information and add to cart functionality to product page

--- src/components/products/ProductPage.js
+++ src/components/products/ProductPage.js
@@ -1,12 +1,15 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useContext } from 'react'
 import axios from 'axios'
-import { Row, Col, Spin, Image, Space, Typography } from 'antd'
+import { Row, Col, Spin, Image, Space, Typography, Button, message } from 'antd'
 import { useLocation, useParams } from 'react-router-dom'
+import { ShoppingCartOutlined, StopOutlined } from '@ant-design/icons'
+import { StoreContext } from 'context/StoreContext'
 
 const { Text, Title } = Typography
 
 const ProductPage = () => {
   const { product } = useLocation()
+  const { addItemToShoppingCart } = useContext(StoreContext)
   const [prod, setProd] = useState(product)
   const params = useParams()
 
@@ -23,9 +26,15 @@ const ProductPage = () => {
     }
   }, [params, product])
 
+  const addToCart = () => {
+    addItemToShoppingCart(prod)
+    message.success(`${prod.name} added to cart`)
+  }
+
   if (prod) {
     console.log(prod)
     const hasDiscount = prod.discount > 0
+    const inStock = prod.stock_quantity > 0
     const price = (() => {
       const p = parseFloat(prod.price).toFixed(2)
       if (hasDiscount) {
@@ -42,6 +51,15 @@ const ProductPage = () => {
       return <div className="product-card-price"><Text strong>{`$${p}`}</Text></div>
     })()
 
+    const stockText = inStock ? (
+      <Text type="success">{`${prod.stock_quantity} in Stock!`}</Text>
+    ) : (
+      <Text type="danger">
+        <StopOutlined />
+        {' Out of Stock'}
+      </Text>
+    )
+
     const page = (
       <>
         <Row
@@ -50,11 +68,25 @@ const ProductPage = () => {
           style={{ width: '100%' }}
         >
           <Col>
-            <Image src={prod.image} />
+            <Image src={prod.image} style={{ maxWidth: '400px' }} />
           </Col>
-          <Col>
-            <Title>{prod.name}</Title>
-            <Text>{price}</Text>
+          <Col style={{ maxWidth: '600px', marginTop: '2em' }}>
+            <Title level={2}>{prod.name}</Title>
+            <Title level={5}>{price}</Title>
+            <Title level={4}>Overview</Title>
+            <Text type="secondary">{prod.description}</Text>
+            <br />
+            <Button
+              style={{ margin: '2em 0 1em 0' }}
+              onClick={addToCart}
+              type="primary"
+              icon={<ShoppingCartOutlined />}
+              disabled={!inStock}
+            >
+              Add to Cart
+            </Button>
+            <br />
+            {stockText}
           </Col>
         </Row>
       </>
