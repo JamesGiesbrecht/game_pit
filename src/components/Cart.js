232a523e781e460d1a6b5d397863175d19b47ee3
JamesGiesbrecht <giesbrechtjames@gmail.com>
2020-11-13 20:13:41 -0600

    Displaying cart items in table

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,16 +1,55 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Table, Typography } from 'antd'
+import { StoreContext } from 'context/StoreContext'
 
 const { Title, Text } = Typography
 
 const Cart = () => {
-  const columns = []
+  const { shoppingCart } = useContext(StoreContext)
+  const columns = [
+    {
+      dataIndex: 'name',
+      title: 'Name',
+      key: 'name',
+    },
+    {
+      title: 'Price',
+      dataIndex: 'price',
+      key: 'price',
+    },
+    {
+      title: 'Quantity',
+      dataIndex: 'quantity',
+      key: 'quantity',
+    },
+    {
+      title: 'Total Price',
+      dataIndex: 'totalPrice',
+      key: 'totalPrice',
+    },
+  ]
+
   const data = []
+
+  shoppingCart.forEach((item) => {
+    const isDuplicateItem = data.some((d) => d.key === item.id)
+    if (!isDuplicateItem) {
+      const filteredCart = shoppingCart.filter((s) => s.id === item.id)
+      const itemPrice = (item.price - item.price * item.discount).toFixed(2)
+      data.push({
+        key: item.id,
+        name: item.name,
+        price: `$${itemPrice}`,
+        quantity: filteredCart.length,
+        totalPrice: `$${filteredCart.length * itemPrice}`,
+      })
+    }
+  })
 
   return (
     <>
       <Title>Your Cart</Title>
-      <Table columns={columns} data={data} />
+      <Table columns={columns} dataSource={data} />
     </>
   )
 }
