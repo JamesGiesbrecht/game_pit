731c988fcd9d3d6ca5e9baa4594b4a1fc5547b9e
JamesGiesbrecht <giesbrechtjames@gmail.com>
2020-11-13 19:17:42 -0600

    Saving and fetching shopping cart from local storage

--- src/context/StoreContext.js
+++ src/context/StoreContext.js
@@ -1,10 +1,14 @@
-import React, { useState, createContext } from 'react'
+import React, { useState, useEffect, createContext } from 'react'
 
 export const StoreContext = createContext()
 
 export default ({ children }) => {
-  const [shoppingCart, setShoppingCart] = useState([])
+  const [shoppingCart, setShoppingCart] = useState(JSON.parse(localStorage.getItem('gamePitShoppingCart')) || [])
   const [breadcrumbs, setBreadcrumbs] = useState(['Home'])
+
+  useEffect(() => {
+    localStorage.setItem('gamePitShoppingCart', JSON.stringify(shoppingCart))
+  }, [shoppingCart])
 
   const store = {
     shoppingCart,
