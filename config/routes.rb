1009d9958ef3825f2787ae0923c15acc5afe7f5c
JamesGiesbrecht <giesbrechtjames@gmail.com>
2020-11-23 20:14:05 -0600

    Adding /api parent route

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   constraints ->(request) { request.format == :json } do
-    resources :categories
-    resources :products
-    resources :pages
+    resources :categories, path: "/api/categories"
+    resources :products, path: "/api/products"
+    resources :pages, path: "/api/pages"
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
