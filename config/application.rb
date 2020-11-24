7055a133befa505d8b505c2dc1925741e7a1204c
JamesGiesbrecht <giesbrechtjames@gmail.com>
2020-11-24 11:58:23 -0600

    Serving static app

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,8 @@ module GamePit
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+    config.serve_static_files = true
+    paths["public"] = File.join "client", "build"
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
